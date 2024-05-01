2ea15ac1cd4234f8ffb8b33642b0ac50cf29bbc2
medkira <mohamed.deftex@gmail.com>
2024-05-01 17:53:58 +0100

    comm interfaces types

--- src/domain/repository/places/DeletePlaceInterface.ts
+++ src/domain/repository/places/DeletePlaceInterface.ts
@@ -1,14 +1,14 @@
-import { PlaceProps } from "@domain/entities/Place";
-import { UseCase } from "../UseCase";
+// import { PlaceProps } from "@domain/entities/Place";
+// import { UseCase } from "../UseCase";
 
-export interface DeletePlaceInterface
-  extends UseCase<DeletePlaceInterface.Request, DeletePlaceInterface.Response> {
-  execute(
-    placeId: DeletePlaceInterface.Request
-  ): Promise<DeletePlaceInterface.Response>;
-}
+// export interface DeletePlaceInterface
+//   extends UseCase<DeletePlaceInterface.Request, DeletePlaceInterface.Response> {
+//   execute(
+//     placeId: DeletePlaceInterface.Request
+//   ): Promise<DeletePlaceInterface.Response>;
+// }
 
-export namespace DeletePlaceInterface {
-  export type Request = string;
-  export type Response = void;
-}
+// export namespace DeletePlaceInterface {
+//   export type Request = string;
+//   export type Response = void;
+// }
--- src/domain/repository/places/GetPlaceByTypeInterface.ts
+++ src/domain/repository/places/GetPlaceByTypeInterface.ts
@@ -1,18 +1,18 @@
-import { PlaceProps, Place } from "@domain/entities/Place";
-import { UseCase } from "../UseCase";
-import { PlaceNotFoundError } from "@application/errors/PlaceNotFoundError";
+// import { PlaceProps, Place } from "@domain/entities/Place";
+// import { UseCase } from "../UseCase";
+// import { PlaceNotFoundError } from "@application/errors/PlaceNotFoundError";
 
-export interface GetPlaceByTypeInterface
-  extends UseCase<
-    GetPlaceByTypeInterface.Request,
-    GetPlaceByTypeInterface.Response
-  > {
-  execute(
-    placeType: GetPlaceByTypeInterface.Request
-  ): Promise<GetPlaceByTypeInterface.Response>;
-}
+// export interface GetPlaceByTypeInterface
+//   extends UseCase<
+//     GetPlaceByTypeInterface.Request,
+//     GetPlaceByTypeInterface.Response
+//   > {
+//   execute(
+//     placeType: GetPlaceByTypeInterface.Request
+//   ): Promise<GetPlaceByTypeInterface.Response>;
+// }
 
-export namespace GetPlaceByTypeInterface {
-  export type Request = Pick<PlaceProps, 'type'>;
-  export type Response = Place | PlaceNotFoundError;
-}
+// export namespace GetPlaceByTypeInterface {
+//   export type Request = Pick<PlaceProps, 'type'>;
+//   export type Response = Place | PlaceNotFoundError;
+// }
